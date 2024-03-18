3ca00ece32d228080728cd8fac50efc93607b0b4
Yadav106 <sahil.jp.yadav@gmail.com>
2024-03-19 00:20:11 +0530

    finished configuring gunicorn and celery

---
+++ api/user_api.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+from flask import current_app as app
+
+
+user_api = Blueprint("user_api", __name__)
--- config/app.py
+++ config/app.py
@@ -12,6 +12,8 @@ from utils.custom_exception import CustomException
 from config.error_handlers import handle_invalid_usage
 from utils.response import set_response
 
+from api import user_api
+
 def create_app(config_obj):
     """
     Creates and configures an instance of a flask application
@@ -27,3 +29,41 @@ def create_app(config_obj):
 
     app.jinja_env.globals.update(isinstance=isinstance)
     app.jinja_env.globals.update(enumerate=enumerate)
+
+    stream_handler = logging.StreamHandler()
+
+    stream_handler.setFormatter(logging.Formatter(
+        fmt='[%(asctime)s] [%(process)d] [%(levelname)s] %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S %z'
+    ))
+
+    stream_handler.setLevel(log_level)
+
+    while app.logger.handlers:
+        app.logger.handlers.pop()
+    app.logger.addHandler(stream_handler)
+
+    register_blueprints(app)
+    register_error_handlers(app)
+    register_extensions(app)
+
+    return app
+
+def register_blueprints(app):
+    app.register_blueprint(user_api.user_api, url_prefix=API_URL_PREFIX)
+
+def register_extensions(app):
+    cors.init_app(app, max_age=3600)
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+def register_error_handlers(app):
+    app.errorhandler(CustomException)(handle_invalid_usage)
+
+app = create_app(ProdConfig)
+celery_app = Celery(
+        __name__, 
+        backend=ProdConfig.CELERY_RESULT_BACKEND,
+        broker=ProdConfig.CELERY_BROKER_URL,
+    )
+app.app_context().push()
